fbdca9e9bc333fbdfde95ba9e4de274e4b976b57
pickagodandpray <tommymaimai@gmail.com>
2017-11-16 10:26:14 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazaar_of_the_Bizarre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world\n" +
+                              "Press any key to close the application...");
+            Console.ReadKey();
+        }
+    }
+}
